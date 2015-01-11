b082979b5642da334b42093e73d136a8547d9d43
Abhishek Bhadange <abhishekbhadange@gmail.com>
2015-01-10 21:42:28 -0800

    N equal parts of String

---
+++ stringsAndThings/src/stringsAndThings/NEqualParts.java
@@ -0,0 +1,22 @@
+// Write a program to print N equal parts of a given string
+
+package stringsAndThings;
+
+public class NEqualParts {
+	public static void main(String[] args) {
+		String str = "ABCDEF";
+		int parts = 3;
+		nEqualParts(str, parts);
+	}
+	public static void nEqualParts(String str, int parts) {
+		int size = str.length();
+		if(size % parts == 0) {
+			int partSize = size / parts;
+			for(int i = 0; i < str.length(); i += partSize) 
+				System.out.println(str.substring(i, i + partSize));
+		}
+		else
+			System.out.println("Invalid Input: String size is not divisible by " + parts);
+			return;
+	}
+}	
